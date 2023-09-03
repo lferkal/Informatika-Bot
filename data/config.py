d446a4151aedd8c47d51456c276132211e323e55
lferkal <ferkido777@gmail.com>
2023-09-03 23:22:22 +0500

    Fix some problems

--- data/config.py
+++ data/config.py
@@ -1,21 +1,22 @@
 from environs import Env
 
-
+# environs kutubxonasidan foydalanish
 env = Env()
 env.read_env()
 
-# BOT_TOKEN = env.str("BOT_TOKEN")  
-# ADMINS = env.list("ADMINS")  
-# IP = env.str("ip")  
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+IP = env.str("ip")  # Xosting ip manzili
 
-# DB_USER = env.str("DB_USER")
-# DB_PASS = env.str("DB_PASS")
-# DB_NAME = env.str("DB_NAME")
-# DB_HOST = env.str("DB_HOST")
+DB_USER = env.str("DB_USER")
+DB_PASS = env.str("DB_PASS")
+DB_NAME = env.str("DB_NAME")
+DB_HOST = env.str("DB_HOST")
 
-import os
-
-BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
-ADMINS = list(os.environ.get("ADMINS"))
-IP = str(os.environ.get("ip"))
-PROVIDER_TOKEN = str(os.environ.get("PROVIDER_TOKEN"))
+# import os
+#
+# BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
+# ADMINS = list(os.environ.get("ADMINS"))
+# IP = str(os.environ.get("ip"))
+# PROVIDER_TOKEN = str(os.environ.get("PROVIDER_TOKEN"))
